8078cfbb34f0109c03e0de07142d737aff8964d9
lvnsx <83145468+lvnsx@users.noreply.github.com>
2026-07-25 22:30:58 +0900

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,10 +1,10 @@
 import type { SiteConfig, ThemeConfig, SettingsConfig, UmamiAnalyticsConfig, AnalyticsConfig } from "../types";
 
 export const SITE: SiteConfig = {
-    website: "https://shannon.github.io/academic-portfolio-astro/",
-    author: "Claude Shannon",
-    desc: "Personal academic portfolio and blog of Claude Shannon, Father of Information Theory.",
-    title: "Claude Shannon",
+    website: "https://lvnsx.github.io/",
+    author: "Love and Sex",
+    desc: "Share my personal experiences.",
+    title: "Love and Sex",
     ogImage: "shannon.webp",
     postPerPage: 5,
     favicon: "/favicon.svg",
